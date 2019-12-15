53a553f7f496ccd8b9b3eba71969c42e9110f4c6
Shaheer Akram <shaheerakram@icloud.com>
2019-12-15 22:42:40 +0500

    babu async hogya display

--- node-mysql-crud/routes/index.js
+++ node-mysql-crud/routes/index.js
@@ -1,26 +1,46 @@
-//const ip = require("ip");
+const ip = require("ip");
+const config = require("../config.json")
+const shaheer = config.shaheer;
 module.exports = {
   getHomePage: (req, res) => {
     let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
     let query_temp = "SELECT * FROM `players_temp` ORDER BY id ASC";
     // execute query
     db.query(query, (err, result) => {
-      db.query(query_temp,(err, result_temp)=>{
-        if(err) {
-          res.redirect('/')
-        }
-        //console.log(ip.address());
-        //console.log(result_temp);
-        res.render("index.ejs", {
-          title: "Welcome to Socka | View Players",
-          players: [...result_temp,...result]
-        });
-        if (err) {
-          res.redirect("/");
-        }
-        //result = [ ...result ,result_temp]
-      })
-      
+      if(ip.address()==="10.75.0.1"){
+        db.query(query_temp,(err, result_temp)=>{
+          if(err) {
+            res.redirect('/')
+          }
+          //console.log(ip.address());
+          //console.log(result_temp);
+          res.render("index.ejs", {
+            title: "Welcome to Socka | View Players",
+            players: [...result_temp,...result]
+          });
+          if (err) {
+            res.redirect("/");
+          }
+          //result = [ ...result ,result_temp]
+        })
+      }
+      else if(ip.address() === shaheer.host){
+        db2.query(query_temp,(err, result_temp)=>{
+          if(err) {
+            res.redirect('/')
+          }
+          //console.log(ip.address());
+          //console.log(result_temp);
+          res.render("index.ejs", {
+            title: "Welcome to Socka | View Players",
+            players: [...result_temp,...result]
+          });
+          if (err) {
+            res.redirect("/");
+          }
+          //result = [ ...result ,result_temp]
+        })
+      }
     });
   },
 };
